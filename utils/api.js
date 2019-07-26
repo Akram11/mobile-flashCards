6e87b094faf7e7fceb89e0363de3345a24fc8485
Akram11 <akram.f11@gmail.com>
2019-07-26 19:53:49 +0200

    changed getInitialData

--- utils/api.js
+++ utils/api.js
@@ -2,13 +2,13 @@ import { AsyncStorage } from 'react-native';
 import initialData from './initialData';
 export const FLASHCARDS_STORAGE_KEY = 'MobileApp:FlashCards';
 
-export const getDecks = () => {
-  checkInitialData();
-  return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY).then(results => {
-    const data = JSON.parse(results);
-    return data;
-  });
-};
+// export const getDecks = () => {
+//   checkInitialData();
+//   return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY).then(results => {
+//     const data = JSON.parse(results);
+//     return data;
+//   });
+// };
 
 export const getInitialData = () => {
   return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY).then(results => {
@@ -18,13 +18,9 @@ export const getInitialData = () => {
       return AsyncStorage.setItem(
         FLASHCARDS_STORAGE_KEY,
         JSON.stringify(initialData)
-      ).then(result => {
-        console.log('dog', result);
-      });
-    } else {
-      console.log('camel', JSON.stringify(data));
+      )
+    } 
       return JSON.stringify(data);
-    }
   });
 };
 
