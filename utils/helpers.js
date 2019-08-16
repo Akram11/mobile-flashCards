1127b453b1228be699de02a56676ae06c1c9761c
Akram11 <akram.f11@gmail.com>
2019-08-16 18:39:16 +0200

    added notifications funcs

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,9 @@
 import {AsyncStorage} from 'react-native'
-import {Notifications, Permission } from 'expo'
+import {Notifications, Permissions } from 'expo'
+import { notification } from 'expo-haptics';
 
 
-
+const NOTIFICATION_KEY = "FlashCards:notifications";
 
 export const generateId = () => {
   return (
@@ -14,3 +15,51 @@ export const generateId = () => {
       .substring(2, 15)
   );
 };
+
+
+export const clearLocalNotification = () => {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+  .then(Notification.cancelAllScheduledNotificationsAsync)
+
+}
+
+export const createNotification = () => {
+  return {
+    title: 'Time for a quiz!',
+    body: "Don't let the day passed without a small quiz",
+    android: {
+      sound: true,
+      sticky: false,
+      priority: 'high',
+      vibrate: true,
+    }
+  }
+}
+
+export const setLocalNotification = () => {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+  .then(JSON.parse)
+  .then((data) => {
+    if (data === null){
+      Permissions.askAsync(Permissions.NOTIFICATIONS)
+      .then((status) => {
+        if (status === 'granted'){
+          Notifications.cancelAllScheduledNotificationsAsync()
+          let tmrw = new Date()
+          tmrw.setDate(tomorrow.getDate() + 1)
+          tmrw.setHours(20)
+          tmrw.setMinutes(0)
+
+          Notifications.scheduleLocalNotificationAsync(
+            createNotification(),
+            {
+              time: tmrw,
+              repeat: 'day', 
+            }
+          )
+          AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+        }
+      })
+    }
+  } )  
+}
